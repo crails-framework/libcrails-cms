d1752cc115e5ae00ecc24ffc68ac0e5fbfdaacc8
plaristote <michael@unetresgrossebite.com>
2023-06-19 21:07:22 +0200

    pages using the document layout now uses a different view

--- libcrails-cms/crails/cms/controllers/page.hpp
+++ libcrails-cms/crails/cms/controllers/page.hpp
@@ -44,10 +44,18 @@ namespace Crails::Cms
     {
       if (model.can_read(Super::get_current_user()))
       {
+        const Layout& layout = model.get_layout();
+        std::string layout_path = layout.get_layout_path();
+        std::string page_view = "pages/show";
+
+        if (!layout_path.length())
+          layout_path = Super::find_settings()->get_layout().get_layout_path();
+        if (layout.get_type() == DocumentLayoutType)
+          page_view = "pages/document";
         Super::prepare_open_graph(model);
-        Super::vars["layout"] = model.get_layout().get_layout_path();
+        Super::vars["layout"] = layout_path;
         Super::vars["render_footer"] = !model.get_has_footer();
-        Super::render("pages/show", {
+        Super::render(page_view, {
           {"page", reinterpret_cast<const Crails::Cms::Page*>(&model)}
         });
       }
