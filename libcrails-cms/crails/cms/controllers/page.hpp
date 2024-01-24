b4a88dc19c371a86529512f777424ae35a40f032
plaristote <michael@unetresgrossebite.com>
2024-01-24 14:32:43 +0100

    port AdminPageController to AdminResourceController

--- libcrails-cms/crails/cms/controllers/page.hpp
+++ libcrails-cms/crails/cms/controllers/page.hpp
@@ -1,46 +1,25 @@
 #pragma once
-#include <crails/odb/controller.hpp>
+#include "resource.hpp"
+#include "../models/page.hpp"
 #include "../views/layout.hpp"
 
 namespace Crails::Cms
 {
   template<typename TRAITS, typename SUPER>
-  class PageController : public Crails::Odb::Controller<SUPER>
+  class PageController : public Crails::Cms::ResourceController<TRAITS, SUPER>
   {
-    typedef Crails::Odb::Controller<SUPER> Super;
-    typedef typename TRAITS::Model Model;
+    typedef Crails::Cms::ResourceController<TRAITS, SUPER> Super;
   public:
     PageController(Crails::Context& context) : Super(context)
     {
     }
 
-    void show()
+    void render_model(const typename Super::Model& model) override
     {
-      using namespace std;
-      shared_ptr<Model> model;
-      auto query = odb::query<Model>::slug == Super::params["slug"].template as<string>();
-
-      Super::database.find_one(model, query);
-      if (model)
-        render_page(*model);
-      else
-        not_found();
+      render_page(model);
     }
 
-    void by_id()
-    {
-      using namespace std;
-      shared_ptr<Model> model;
-      auto query = odb::query<Model>::id == Super::params["id"].template as<Crails::Odb::id_type>();
-
-      Super::database.find_one(model, query);
-      if (model)
-        Super::redirect_to(Crails::HttpStatus::permanent_redirect, "/page/" + model->get_slug());
-      else
-        not_found();
-    }
-
-    virtual void render_page(const Model& model)
+    virtual void render_page(const typename Super::Model& model)
     {
       if (model.can_read(Super::get_current_user()))
       {
@@ -61,29 +40,6 @@ namespace Crails::Cms
       }
       else
         Super::respond_with(Crails::HttpStatus::forbidden);
-    }
-
-    virtual void not_found()
-    {
-      Super::respond_with(Crails::HttpStatus::not_found);
-    }
-
-    bool must_protect_from_forgery() const override
-    {
-      if (Super::get_action_name() == "preview")
-        return false;
-      return Super::must_protect_from_forgery();
-    }
-
-    void preview()
-    {
-      Model model;
-      auto settings = Super::find_settings();
-
-      model.edit(Super::params[Crails::Cms::Page::scope]);
-      Super::prepare_open_graph(model);
-      Super::vars["preview"] = true;
-      render_page(model);
     }
   };
 }
