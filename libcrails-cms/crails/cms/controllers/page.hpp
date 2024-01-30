fc18bb4929699f41164e6742adc96c50c8c233c4
plaristote <michael@unetresgrossebite.com>
2024-01-30 01:29:10 +0100

    adds page injection

--- libcrails-cms/crails/cms/controllers/page.hpp
+++ libcrails-cms/crails/cms/controllers/page.hpp
@@ -2,6 +2,10 @@
 #include "resource.hpp"
 #include "../models/page.hpp"
 #include "../views/layout.hpp"
+#include <boost/lexical_cast.hpp>
+#include <crails/any_cast.hpp>
+#include <crails/renderer.hpp>
+#include <crails/logger.hpp>
 
 namespace Crails::Cms
 {
@@ -40,6 +44,38 @@ namespace Crails::Cms
       }
       else
         Super::respond_with(Crails::HttpStatus::forbidden);
+    }
+
+    static std::string injectable_page(const Crails::SharedVars& vars)
+    {
+      using namespace std;
+      Crails::Odb::Connection database;
+      shared_ptr<typename Super::Model> model;
+      const Crails::Renderer* renderer;
+      Crails::Odb::id_type page_id = boost::lexical_cast<Crails::Odb::id_type>(
+        Crails::any_cast(vars.at("id"))
+      );
+
+      database.find_one(model, odb::query<typename Super::Model>::id == page_id);
+      if (model)
+      {
+        Crails::RenderString output;
+        Crails::SharedVars view_vars(vars);
+        const Layout& layout = model->get_layout();
+        std::string page_view = "pages/show";
+
+        if (layout.get_type() == DocumentLayoutType)
+          page_view = "pages/document";
+        view_vars.erase("layout");
+        view_vars["render_footer"] = false;
+        view_vars["page"] = reinterpret_cast<const Crails::Cms::Page*>(model.get());
+        renderer = Crails::Renderer::pick_renderer(page_view, "text/html");
+        renderer->render_template(page_view, output, view_vars);
+        return string("<!-- Injected page " + model->get_slug() + " --!>")
+          + string(output.value());
+      }
+      return string("<!-- Page not found --!>");
+      
     }
   };
 }
