5526aa913fe1ed049271ff78912ca071e6c43238
plaristote <michael@unetresgrossebite.com>
2024-01-30 03:30:53 +0100

    adds customizable height/width on page editor pictures

--- webpackage/admin/page_editor/image_component_editor.js
+++ webpackage/admin/page_editor/image_component_editor.js
@@ -3,6 +3,8 @@ import GridComponentEditor from "./grid_component_editor.js";
 export default class extends GridComponentEditor() {
   initializeProperties() {
     this.properties.image = { type: "image", target: this.image, attribute: "src" };
+    this.properties.width = { type: "number", target: this, attribute: "imageWidth", optional: true };
+    this.properties.height = { type: "number", target: this, attribute: "imageHeight", optional: true };
     super.initializeProperties();
   }
 
@@ -16,6 +18,30 @@ export default class extends GridComponentEditor() {
   bindElements() {
     this.image = this.root.querySelector("img");
     super.bindElements();
+  }
+
+  get imageWidth() {
+    return this.root.dataset.width;
+  }
+
+  get imageHeight() {
+    return this.root.dataset.height;
+  }
+
+  set imageWidth(value) {
+    this.root.dataset.width = value;
+    if (value != null)
+      this.image.setAttribute("width", value);
+    else
+      this.image.removeAttribute("width");
+  }
+
+  set imageHeight(value) {
+    this.root.dataset.height = value;
+    if (value != null)
+      this.image.setAttribute("height", value);
+    else
+      this.image.removeAttribute("height");
   }
 }
 
